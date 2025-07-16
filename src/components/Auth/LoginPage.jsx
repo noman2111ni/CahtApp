8ef9bdc638eb52e316da5813cd7361d6e4928f6b
noman shaber <nimrashaheen451@gmail.com>
2025-07-16 23:40:38 +0500

    updates

--- src/components/Auth/LoginPage.jsx
+++ src/components/Auth/LoginPage.jsx
@@ -14,7 +14,7 @@ const LoginPage = ({ setuser }) => {
     };
 
     return (
-        <div className="flex items-center justify-center min-h-screen bg-gray-100 px-4">
+        <div className="flex items-center justify-center   px-4">
             <div className="w-full max-w-md bg-white p-8 rounded-lg shadow-md">
                 <h2 className="text-2xl font-bold text-center text-gray-800 mb-6">
                     Welcome to the Chat Application
